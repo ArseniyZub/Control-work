0d4997a2593787f8121ae9b8e89eb78e2c4ed8d3
ArseniyZub <zubkov_000@list.ru>
2022-12-01 15:42:30 +0300

    implemented sorting by the number of characters in an array element

--- Task/Program.cs
+++ Task/Program.cs
@@ -9,5 +9,21 @@
     return arr;
 }
 
+String[] ChangedArray(String[] arr)
+{
+    List<String> change = new List<String>();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i].Length <= 3)
+        {
+            change.Add(arr[i]); 
+        }
+    }
+    String[] changeArr = change.ToArray();
+    return changeArr;
+} 
+
 String[] arr =  FillArray(3);
 Console.WriteLine(String.Join(", ", arr));
+String[] changeArr =  ChangedArray(arr);
+Console.WriteLine(String.Join(", ", changeArr));
