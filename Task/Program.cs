bcf7086d0ff0aa40aeaef0dcbf80f0d43e158bb7
ArseniyZub <zubkov_000@list.ru>
2022-12-01 15:18:28 +0300

    implemented array filling

---
+++ Task/Program.cs
@@ -0,0 +1,13 @@
+﻿String[] FillArray(int size)
+{
+    String[] arr = new String[size];
+    for (int i = 0; i < size; i++)
+    {
+        String text = Console.ReadLine()!;
+        arr[i] = text;
+    }
+    return arr;
+}
+
+String[] arr =  FillArray(3);
+Console.WriteLine(String.Join(", ", arr));
